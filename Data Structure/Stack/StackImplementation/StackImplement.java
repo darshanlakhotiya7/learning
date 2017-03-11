517f591ad8b3540699ec94e8bc98e1691a23acd7
Darshan Lakhotiya <darshanlakhotiya7@gmail.com>
2017-03-11 18:47:10 +0530

    Update StackImplement.java

--- Data Structure/Stack/StackImplementation/StackImplement.java
+++ Data Structure/Stack/StackImplementation/StackImplement.java
@@ -1,60 +1,50 @@
+
 public class StackImplement {
 
 	protected int stackarray[];
-	protected int size,top,len;
-	
+	protected int size, top, len;
+
 	public StackImplement(int size) {
-		this.size=size;
-		stackarray=new int[size];
-		top=-1;
-	}
-	public void push(int data) 
-	{
-			top++;
-			stackarray[top]=data;
+		this.size = size;
+		stackarray = new int[size];
+		top = -1;
 	}
 
-	public boolean isEmpty() 
-	{
-		if(top==-1)
-		{
+	public void push(int data) {
+		top++;
+		stackarray[top] = data;
+	}
+
+	public boolean isEmpty() {
+		if (top == -1) {
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
 
-	public boolean isFull() 
-	{
-		if(top==size-1)
-		{
+	public boolean isFull() {
+		if (top == size - 1) {
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
-	
-	public int pop() 
-	{
-		int	data=stackarray[top];
+
+	public int pop() {
+		int data = stackarray[top];
 		top--;
 		return data;
 	}
 
-	public int peek() 
-	{
-		int	data=stackarray[top];
+	public int peek() {
+		int data = stackarray[top];
 		return data;
 	}
-	public void display() 
-	{
-		for(int i=top;i>=0;i--)
-		{
-			System.out.print(stackarray[i]+"\t");
+
+	public void display() {
+		for (int i = top; i >= 0; i--) {
+			System.out.print(stackarray[i] + "\t");
 		}
 	}
 }
