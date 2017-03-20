7d832045db97b7367cf8dfb63f6b038a9c7fab69
Darshan Lakhotiya <darshanlakhotiya7@gmail.com>
2017-03-20 20:47:58 +0530

    Update DivisibleByEleven.java

--- Aptitude/NumberSystem/DivisibleByEleven/DivisibleByEleven.java
+++ Aptitude/NumberSystem/DivisibleByEleven/DivisibleByEleven.java
@@ -1,37 +1,25 @@
-/*
- * 
- Check if a large number is divisible by 11 or not
+public class DivisibleByEleven {
+	String value;
+	int length, sumeven = 0, sumodd = 0;
 
- Given a number, the task is that we divide number by 11. 
- The input number may be large and it may not be possible to store even if we use long long int.
+	public DivisibleByEleven(String value) {
+		this.value = value;
+		length = value.length();
+	}
 
- Examples:
-
- Input : n = 76945
- Output : Yes
-
- Input  : n = 1234567589333892
- Output : Yes
-
- Input  : n = 363588395960667043875487
- Output : No
-
- */
-
-import java.util.Scanner;
-
-public class Test {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		try {
-			System.out.println("Enter the number is :");
-			String input = sc.next();
-			DivisibleByEleven diveleven = new DivisibleByEleven(input);
-			diveleven.check();
-		} catch (Exception e) {
-			System.out.println(e);
-		} finally {
-			sc.close();
+	public void check() {
+		for (int i = 0; i < length; i++) {
+			int number = Character.getNumericValue(value.charAt(i));
+			if (i % 2 == 0) {
+				sumeven = sumeven + number;
+			} else {
+				sumodd = sumodd + number;
+			}
+		}
+		if ((sumodd - sumeven) % 11 == 0) {
+			System.out.println(value + " is divisible for 11");
+		} else {
+			System.out.println(value + " not divisible for 11");
 		}
 	}
 }
